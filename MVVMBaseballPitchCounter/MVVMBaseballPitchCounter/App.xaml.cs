18f0921a034dcd9ec2c58e9d9afa15ad4d3b3a1b
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2022-11-07 19:55:51 -0500

    MVVM Pitch Counter Lecture

---
+++ MVVMBaseballPitchCounter/MVVMBaseballPitchCounter/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MVVMBaseballPitchCounter;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MVVMBaseballPitchCounter/MVVMBaseballPitchCounter/Models/Pitcher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MVVMBaseballPitchCounter.Models
+{
+    public class Pitcher:INotifyPropertyChanged
+    {
+		private int _balls;
+
+		public int Balls
+		{
+			get { return _balls; }
+			set { _balls = value;
+				TotalPitches = _balls + _strikes;
+				OnPropertyChanged("Balls");
+			}
+		}
+
+		private int _strikes;
+
+		public int Strikes
+		{
+			get { return _strikes; }
+			set { _strikes = value;
+                TotalPitches = _balls + _strikes;
+                OnPropertyChanged("Strikes");
+            }
+		}
+
+		private int _total;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string Propertyname)
+		{
+			if(PropertyChanged != null)
+			{
+				PropertyChangedEventArgs args = new PropertyChangedEventArgs(Propertyname);
+				this.PropertyChanged(this, args);
+
+			}
+		}
+
+		public int TotalPitches
+		{
+			get { return _total; }
+			set { _total = value;
+				OnPropertyChanged("TotalPitches");
+			}
+		}
+
+		public Pitcher()
+		{
+			_balls = 0;	
+			_strikes = 0;
+			_total = 0;
+		}
+
+
+	}
+}
