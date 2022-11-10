2dcbd849279b1b4f8edbbee288dc8f87b3b0ec59
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2022-11-10 14:52:07 -0500

    MVVM update
    I created a copy of the MVVM lecture. I want to practice building a settings page.

---
+++ MVVMPitchCounter/MVVMBaseballPitchCounter/MVVMBaseballPitchCounter/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MVVMBaseballPitchCounter;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MVVMPitchCounter/MVVMBaseballPitchCounter/MVVMBaseballPitchCounter/ViewModels/PitcherViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMBaseballPitchCounter.Models;
+using System.ComponentModel;
+
+namespace MVVMBaseballPitchCounter.ViewModels
+{
+    public class PitcherViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+		protected void NotifyPropertyChanged(string propertyName)
+		{
+			if(PropertyChanged != null)
+			{
+				PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+				this.PropertyChanged(this, args);
+			}//sends message to the model
+		}
+
+		public Command AddBallCommand { get; private set; }
+		public Command AddStrikeCommand { get; private set; }
+
+		public void AddBall()
+		{
+			pitcher.Balls++;
+		}
+
+		public void AddStrike()
+		{
+			pitcher.Strikes++;
+		}
+
+		private Pitcher pitcher;
+
+		public Pitcher ThePitcher
+		{
+			get { return pitcher; }
+			set { pitcher = value; }
+		}
+
+		public PitcherViewModel()
+		{
+			AddBallCommand = new Command(AddBall);
+			AddStrikeCommand = new Command(AddStrike);
+			pitcher = new Pitcher();
+		}
+
+	}
+}
