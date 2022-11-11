821faf73e7badaac91fd6ffceda26f4689ed0922
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2022-11-11 10:01:00 -0500

    color behind

--- MVVMPitchCounter/MVVMBaseballPitchCounter With Settings/MVVMBaseballPitchCounter/Views/Settings.xaml.cs
+++ MVVMPitchCounter/MVVMBaseballPitchCounter With Settings/MVVMBaseballPitchCounter/Views/Settings.xaml.cs
@@ -8,15 +8,18 @@ public partial class Settings : ContentPage
 
     void OnTeamChange(object sender, CheckedChangedEventArgs args)
 	{
-		
+		Color text, back;
 		if (sender.Equals(Astros))
 		{
 			//DisplayAlert("Team", "Astros picked", "OK");
-			Resources["TeamText"] = Color.FromHex(Resources["AstrosText"].ToString());
-            Resources["TeamBack"] = Color.FromHex(Resources["AstrosBack"].ToString());
-            
+
+			text = (Color)Application.Current.Resources["AstrosText"];
+            back = (Color)Application.Current.Resources["AstrosBack"];
+            Resources["TeamText"] = text;
+            Resources["TeamBack"] = back;
         }
-	}
+        
+    }
 	public Settings()
 	{
 		InitializeComponent();
